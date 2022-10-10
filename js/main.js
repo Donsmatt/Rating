43b2cf7fab993d8a78762787110c06fdef85ac9d
Collins Adaigho <donsmatt44@gmail.com>
2022-10-10 23:32:37 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const rate = document.querySelectorAll("#rate");
+rate.forEach((r) => r.addEventListener("click", selectRate));
+
+function removeClass() {
+  rate.forEach((r) => r.classList.remove("bg-orange"));
+}
+
+function selectRate(e) {
+  removeClass();
+
+  this.classList.add("bg-orange");
+
+  const button = document.getElementById("button");
+  button.innerText = `You selected ${this.innerText} out of 5`;
+}
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const hide = document.getElementById("hide");
+  hide.classList.remove("hide");
+
+  const show = document.getElementById("show");
+  show.replaceWith(hide);
+});
